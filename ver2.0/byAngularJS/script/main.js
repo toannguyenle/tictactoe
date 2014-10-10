7c66e41459718a78e5dedd88e03d0bb9e304e81b
toannguyenle <toan@me.com>
2014-10-09 19:26:52 -0700

    Make work in Angular Ver 2.0

--- ver2.0/byAngularJS/script/main.js
+++ ver2.0/byAngularJS/script/main.js
@@ -15,90 +15,81 @@
   {name:"c8",status:"0"}
   ];
 
-  //GlobalArray to store User's moves on board, each user board is an array in a global array of 2 elements
-  $scope.userBoard = [["0","0","0","0","0","0","0","0","0"],["0","0","0","0","0","0","0","0","0"]];
+  //Array to store User's moves on board, each user board is an array in a global array of 2 elements
+  var userBoard = [["0","0","0","0","0","0","0","0","0"],["0","0","0","0","0","0","0","0","0"]];
   // Standard set of winning possibilities - only 8
-  $scope.winningMoves = ["111000000","000111000","000000111","100100100","010010010","001001001","100010001","001010100"];
+  var winningMoves = ["111000000","000111000","000000111","100100100","010010010","001001001","100010001","001010100"];
   // Counter to make sure alternate between noughts and crosses
   $scope.count = 0;
   // Tie in case
-  $scope.tie = false;
+  var tie = false;
+
+  // Tile Style
+  $scope.tileStyle = "";
 
   // THE JUDGE
-  // $scope.judgeGame = function (obj){
-  //   var result = false;
-  //   for (var i = 0; i < winningMoves.length; i++) {
-  //     var winPoint = 0;
-  //     // break into array
-  //     var winMove = winningMoves[i].split("");
-  //     for (var j = 0; j < winMove.length; j++) {
-  //       if (winMove[j]*obj[j] == 1) {
-  //         winPoint = winPoint + 1;
-  //       };
-  //       if (winPoint == 3) {
-  //         // to break from both loop if we found a winning move
-  //         result = true;
-  //         i = winningMoves.length;
-  //         break;
-  //       };
-  //     };
-  //       //Tie
-  //       if (i==(winningMoves.length-1)){
-  //         tie=true;
-  //       };
-  //   };
-  //   return result;
-  // };
+  var judgeGame = function (obj){
+    var result = false;
+    for (var i = 0; i < winningMoves.length; i++) {
+      var winPoint = 0;
+      // break into array
+      var winMove = winningMoves[i].split("");
+      for (var j = 0; j < winMove.length; j++) {
+        if (winMove[j]*obj[j] == 1) {
+          winPoint++;
+        }
+        console.log(winPoint);
+        if (winPoint == 3) {
+          // to break from both loop if we found a winning move
+          result = true;
+          i = winningMoves.length;
+          break;
+        }
+      };
+        //Tie
+        if (i==(winningMoves.length-1)){
+          tie=true;
+        };
+    };
+    return result;
+  };
 
   //chooseTile
   $scope.chooseTile = function(thisTile){
-
     // to alternate between noughts and crosses
     if (($scope.count % 2) == 0) {
-      thisTile.status = 'X';}
-    else {
+      thisTile.status = 'X';
+      // Update user1 board
+      userBoard[1][this.$index] = 1;}
+    else 
+      {
       thisTile.status = 'O';
-    }
+      // Update user2 board
+      userBoard[0][this.$index] = 1;}
     $scope.count++;
-    // person judging
-    // if ($scope.judgeGame($scope.userBoard[0])) {
-    //   alert("Player O wins!");
-    //   tie = false;
-    // }
-    // if ($scope.judgeGame($scope.userBoard[1])) {
-    //   alert("Player X wins!");
-    // };
-    // if (($scope.count==9)&($scope.tie)) {
-    //   alert("TIE!");
-    //   $scope.tied = false;
-    // };
+
+    // Let's find out the winner
+    if (judgeGame(userBoard[1])) {
+      alert("Player X wins!");
+      tie = false;
+    }
+    if (judgeGame(userBoard[0])) {
+      alert("Player O wins!");
+      tie = false;
+    };
+    if (($scope.count==9)&(tie)) {
+      alert("TIE!");
+    };
   };
 
-
-  // CLICK to Change AND Retain Value
-  $scope.clickTile = function (){
-  for (var cellId = 0; cellId < 9; cellId++) {
-    // var currentCell = document.getElementById("c"+cellId);
-    // currentCell.addEventListener("click", chooseTile());
-    // };
-  };
 
   //  MOUSE OVER to Change Color AND returns Color when MOUSE OUT
-  // $scope.hoverTile = function (){
-  // var count = 0;
-  // for (var cellId = 0; cellId < 9; cellId++) {
-  //   var currentCell = document.getElementById("c"+cellId);
-  //   // MOUSE OVER TO SEE WHATS THE SELECTION IS
-  //   currentCell.addEventListener("mouseover", function()
-  //   {
-  //     this.style.backgroundColor = "pink";
-  //   });
-  //   // MOUSE OUT TO RETURN ORIGINAL COLOR
-  //   currentCell.addEventListener("mouseout", function()
-  //   {
-  //     this.style.backgroundColor = "red";
-  //   });
-  //   };
-  // };
+  $scope.hoverTile = function (){
 
-}});
+    // MOUSE OVER TO SEE WHATS THE SELECTION IS
+      // $scope.tileStyle = {"background-color" : "pink"};
+
+    // MOUSE OUT TO RETURN ORIGINAL COLOR
+      // $scope.tileStyle = {"background-color" : "red"};
+  };
+});
